8026e01b3a8a1ddf2dd74e7e0e9cfba27da6dda4
Rowing0914 <kosakaboat@gmail.com>
2020-05-18 16:43:00 +0900

    Fixed the form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable the stupid cache mechanism
@@ -8,9 +8,21 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable the stupid cache mechanis
 def index():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
     return render_template('form.html')
+
+
+@app.route('/register', methods=["POST"])
+def register():
+    display_name = request.form['display-name']
+    price = request.form['price']
+    description = request.form['description']
+    email = request.form['email']
+    psw = request.form['psw']
+    print(display_name, price, description, email, psw)
+    return redirect('/')
 
 
 if __name__ == "__main__":
